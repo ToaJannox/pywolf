57af80df250b1e36135c2b688b6326e9540fe88c
Montfermé Robin <robin.montferme@gmail.com>
2024-01-10 13:59:48 +0100

    more refactor

--- src/game.py
+++ src/game.py
@@ -1,164 +1,172 @@
 import math
-from player import *
+
+from player import Player
 from random import shuffle
 
 from time import sleep
-from phase import Phase
 
-from phase import Phase
-from roleType import RoleType 
 
-class
+from enums.phase import Phase
+from enums.roleType import RoleType
+
+DEFAULT_MAX_TURNS = 10
+DELAY = 1
+
 
 class Game:
     def __init__(self):
-        self.playerList = []
-        self.playerAmount = 0
+        self.player_list = []
+        self.player_amount = 0
         self.phase = Phase.SETUP
-        self.roleList = dict()
+        self.role_list = dict()
+        self.victims = []
+        self.max_turns = DEFAULT_MAX_TURNS
+        self.current_turn = 0
+        self.running = False
+        self.player_factions = {"villagers": 0, "werewolves": 0, "others": 0}
+        self.winning_faction = None
 
     def setup(self, roleList=[("Villager", 7), ("Werewolf", 3)]):
         print("Setting game up.")
-        self.playerList = []
+        self.player_list = []
         for role, amount in roleList:
-            if role in villagerRoles:
-                villagerAmount += amount
-                for i in range(0,amount):
-                    playerList.append(villagerRoles[role]())
-            elif role in werewolfRoles:
-                werewolfAmount += amount
-                for i in range(0,amount):
-                    playerList.append(werewolfRoles[role]())
-            elif role in ambiguousRoles:
-                ambiguousRoles += amount
-                for i in range(0,amount):
-                    playerList.append(ambiguousRoles[role]())
-            elif role in lonerRoles:
-                lonerAmount += amount
-                for i in range(0,amount):
-                    playerList.append(lonerRoles[role]())
-            elif role in specialRoles:
-                specialAmount +=amount
-                for i in range(0,amount):
-                    playerList.append(specialRoles[role]())
-            else: #if role is unknown put villager instead
-                raise ValueError("Unknown role",role)
-            
-        shuffle(playerList)
+            pass
+            # if role in villagerRoles:
+            #     villagerAmount += amount
+            #     for i in range(0, amount):
+            #         player_list.append(villagerRoles[role]())
+            # elif role in werewolfRoles:
+            #     werewolfAmount += amount
+            #     for i in range(0, amount):
+            #         player_list.append(werewolfRoles[role]())
+            # elif role in ambiguousRoles:
+            #     ambiguousRoles += amount
+            #     for i in range(0, amount):
+            #         player_list.append(ambiguousRoles[role]())
+            # elif role in lonerRoles:
+            #     lonerAmount += amount
+            #     for i in range(0, amount):
+            #         player_list.append(lonerRoles[role]())
+            # elif role in specialRoles:
+            #     specialAmount += amount
+            #     for i in range(0, amount):
+            #         player_list.append(specialRoles[role]())
+            # else:  # if role is unknown put villager instead
+            #     raise ValueError("Unknown role", role)
+
+        shuffle(self.player_list)
         i = 0
-        for player in playerList:
-            player.name = str(i)+"p"
+        for player in self.player_list:
+            player.name = str(i) + "p"
             i += 1
 
+    def first_night(self):
+        self.night()
+
+    def night(self):
+        self.victims.append(self.wolfVote())
+        self.phase = Phase.DAY
+
+    def day(self):
+        self.current_turn += 1
+        self.victims.append(self.dayVote())
+        self.phase = Phase.NIGHT
 
     def play(self):
-        max_turn = 10
-        cpt = 0
-        running = True
-        victims = []
-        while running:
-            if phase == Phase.SETUP:
+        self.running = True
+        while self.running:
+            if self.phase == Phase.SETUP:
                 print("\nSetup\n")
-                phase = Phase.FIRST_NIGHT
-            elif phase == Phase.FIRST_NIGHT:
+                self.phase = Phase.FIRST_NIGHT
+            elif self.phase == Phase.FIRST_NIGHT:
                 print("\nFirst Night\n")
-                self.wolfSetup()
-                victims.append(self.wolfVote())
-                phase = Phase.DAY
-            elif phase == Phase.NIGHT:
+                self.first_night()
+            elif self.phase == Phase.NIGHT:
                 print("\nNight\n")
-                victims.append(self.wolfVote())
-                phase = Phase.DAY
-            elif phase == Phase.DAY:
+                self.night()
+            elif self.phase == Phase.DAY:
                 print("Day\n")
-                cpt += 1
-                victims.append(self.dayVote())
-                phase = Phase.NIGHT
-            killVictims(victims)
-            victims = []
-            if cpt == max_turn or villagerAmount == 0 or werewolfAmount == 0:
-                running = False
+                self.day()
+            self.killVictims()
+            self.victims = []
+            if self.current_turn >= self.max_turns or self.check_game_end():
+                self.running = False
             print("\nRemaining players: \n")
-            for p in playerList:
+            for p in self.player_list:
                 p.display()
                 p.votes = 0
             print("-----------------------------------")
-            sleep(delay)
+            sleep(DELAY)
 
         print("Game ended")
-        print("Villager alive " + str(villagerAmount))
-        print("Werewolves alive " + str(werewolfAmount))
-        if villagerAmount == 0:
+        print(f"Villager alive {self.player_factions['villagers']}")
+        print(f"Werewolves alive {self.player_factions['werewolves']}")
+        if self.winning_faction == RoleType.VILLAGER:
             print("The wolves have won!!")
-        elif werewolfAmount == 0:
+        elif self.winning_faction == RoleType.WEREWOLF:
             print("The villagers have won!!")
+        else:
+            print("Player win")
 
+    def count_alive_player_per_factions(self):
+        for p in self.player_list:
+            if p.role_type == RoleType.VILLAGER:
+                self.player_factions["villagers"].append(p)
+            elif p.role_type == RoleType.WEREWOLF:
+                self.player_factions["werewolves"].append(p)
+            else:
+                self.player_factions["others"].append(p)
 
-    def wolfSetup(self):
-        wolfList = []
-        for p in playerList:
+    def check_game_end(self):
+        return self.player_factions["werewolves"] >= self.player_factions["villagers"]
+
+    def wolf_setup(self):
+        wolf_list = []
+        for p in self.player_list:
             if p.role == "Werewolf":
-                wolfList.append(p)
-        for p in wolfList:
-            p.allies = [a for a in wolfList if a != p]
+                self.wolf_list.append(p)
+        for p in wolf_list:
+            p.allies = [a for a in wolf_list if a != p]
 
-
-    def wolfVote(self):
-        global playerList
+    def wolf_vote(self):
         result = 0
-        highestVote = 0
-        for player in playerList:
+        highest_vote = 0
+        for player in self.player_list:
             if player.role == "Werewolf" and player.alive:
-                vote = player.vote(playerList)
-                playerList[vote].votes += 1
-        for player in playerList:
-            if player.role == "Villager":
-                if player.votes > highestVote:
-                    highestVote = player.votes
-                    result = playerList.index(player)
-        print("Wolfs victim: "+playerList[result].name)
+                vote = player.vote(self.player_list)
+                self.player_list[vote].votes += 1
+        for player in self.player_list:
+            if player.role == "Villager" and player.votes > highest_vote:
+                highest_vote = player.votes
+                result = self.player_list.index(player)
+        print(f"Wolfs victim: {self.player_list[result].name}")
         return result
 
-
-    def dayVote(self):
-        global playerList
+    def day_vote(self):
         result = 0
-        highestVote = 0
-        for player in playerList:
+        highest_vote = 0
+        for player in self.player_list:
             if player.alive:
-                vote = player.vote(playerList)
+                vote = player.vote(self.player_list)
                 if player.isCaptain:
-                    playerList[vote].votes += 2
+                    self.player_list[vote].votes += 2
                 else:
-                    playerList[vote].votes += 1
-        for player in playerList:
-            if player.votes > highestVote:
-                highestVote = player.votes
-                result = playerList.index(player)
-        print("Chosen villager: "+playerList[result].name +
-            " with "+str(playerList[result].votes)+" votes")
+                    self.player_list[vote].votes += 1
+        for player in self.player_list:
+            if player.votes > highest_vote:
+                highest_vote = player.votes
+                result = self.player_list.index(player)
+        print(
+            f"Chosen villager: {self.player_list[result].name} with  {self.player_list[result].votes} votes"
+        )
         return result
 
+    def kill_victims(self):
+        for victim in self.victims:
+            p = self.player_list[victim]
+            p.death()
+        self.count_alive_player_per_factions()
 
-    def killVictims(self,list):
-        global playerList, werewolfAmount, villagerAmount
-        for victim in list:
-            p = playerList[victim]
-            p.display
-            p.alive = False
-            print("Player " + p.name + " is now dead and was a " + p.role)
-        villagers = 0
-        wolves = 0
-        for p in playerList:
-            if p.alive:
-                if p.role == "Villager":
-                    villagers += 1
-                else:
-                    wolves += 1
-        if villagers < villagerAmount:
-            villagerAmount = villagers
-        if wolves < werewolfAmount:
-            werewolfAmount = wolves
 
 g = Game()
 
--- src/player.py
+++ src/player.py
@@ -1,141 +1,166 @@
-
 from random import randint
-from roleType import RoleType
+from enums.roleType import RoleType
+
+
 class Player:
     def __init__(self):
-        self.name =""
+        self.name = ""
         self.role = "role"
-        self.roleType = None
+        self.role_type = None
         self.votes = 0
         self.alive = True
-        self.hasPower = False
-        self.isCaptain = False
+        self.has_power = False
+        self.is_captain = False
         self.memory = []
         self.lover = None
+        self.role_is_revealed = False
 
     def display(self):
         if self.alive:
             print(self.name)
         else:
-            print(self.name +"("+self.role +")")
-    def checkRoleIs(self,role):
-        return (type(self.role) is role)
+            print(self.name + "(" + self.role + ")")
 
-    def vote(self,list):
-        chosenPlayer = self
+    def check_role(self, role):
+        return type(self.role) is role
+
+    def vote(self, list):
+        chosen_player = self
         choice = list.index(self)
-        while (chosenPlayer == self) or not chosenPlayer.alive:
-            choice = randint(0,len(list)-1)
-            chosenPlayer = list[choice]
+        while (chosen_player == self) or not chosen_player.alive:
+            choice = randint(0, len(list) - 1)
+            chosen_player = list[choice]
         return choice
+
     def death(self):
-        pass
+        self.alive = False
+        self.role_is_revealed = True
+        if self.lover:
+            self.lover.death()
+        print(f"Player {self.name} is now dead and was a {self.role}")
 
 
 class Villager(Player):
     def __init__(self):
-        super().__init__()
         self.role = "Villager"
         self.roleType = RoleType.VILLAGER
 
-        
-class Werewolf(Player): 
+
+class VillagerVillager(Player):
     def __init__(self):
-        super().__init__()
-        self.role ="Werewolf"
+        self.role = "Villager-Villager"
+        self.roleType = RoleType.VILLAGER
+        self.role_is_revealed = True
+
+
+class Werewolf(Player):
+    def __init__(self):
+        self.role = "Werewolf"
         self.roleType = Werewolf
         self.allies = []
-    def vote(self,list):
-        chosenPlayer = self
+
+    def vote(self, list):
+        chosen_player = self
         choice = list.index(self)
-        while ((chosenPlayer == self) or not chosenPlayer.alive) or (chosenPlayer in self.allies):
-            choice = randint(0,len(list)-1)
-            chosenPlayer = list[choice]
+        while ((chosen_player == self) or not chosen_player.alive) or (
+            chosen_player in self.allies
+        ):
+            choice = randint(0, len(list) - 1)
+            chosen_player = list[choice]
         return choice
+
 
 class Ambiguous(Player):
     def __init__(self):
-        super().__init__()
         self.name = "Ambiguous"
-    def vote(self,list):
+
+    def vote(self, list):
         pass
+
+
 class Loner(Player):
     def __init__(self):
-        super().__init__()
         self.name = "Loner"
-    def vote(self,list):
+
+    def vote(self, list):
         pass
-#TODO finish  test FortuneTeller
+
+
+# TODO finish  test FortuneTeller
 class FortuneTeller(Villager):
     def __init__(self):
-        super().__init__()
-        self.role="Fortune Teller"
+        self.role = "Fortune Teller"
         self.hasPower = True
-    def vote(self,list):
+
+    def vote(self, list):
         if not self.memory:
             choice = super.vote(list)
         else:
             targets = []
-            for player,role in self.memory:
+            for player, role in self.memory:
                 if role == "Werewolf":
                     targets.append(player)
-            choice = list.index(targets[randint(0,len(targets)-1)])
+            choice = list.index(targets[randint(0, len(targets) - 1)])
         return choice
-    def tellFortune(self,list):
+
+    def tellFortune(self, list):
         chosenTarget = self
-        while(chosenTarget == self or (chosenTarget,chosenTarget.role) in self.memory):
-            chosenTarget = list[randint(0,len(list)-1)]
-        print("The Fortune Teller saw that "+chosenTarget.name + " is a "+chosenTarget.role)
+        while chosenTarget == self or (chosenTarget, chosenTarget.role) in self.memory:
+            chosenTarget = list[randint(0, len(list) - 1)]
+        print(
+            "The Fortune Teller saw that "
+            + chosenTarget.name
+            + " is a "
+            + chosenTarget.role
+        )
+
 
 villagerRoles = {
-    "Villager":Villager,
-    "Villager-Villager":Player,
-    "Fortune Teller":FortuneTeller,
-    "Cupid":Player,
-    "Witch":Player,
-    "Hunter":Player,
-    "Little Girl":Player,
-    "Guard":Player,
-    "Ancient":Player,
-    "Scapegoat":Player,
-    "Idiot":Player,
-    "Savior":Player,
-    "Two Sisters":Player,
-    "Three Brothers":Player, 
-    "Devoted Maid":Player,
-    "Fox":Player, 
-    "Bear Tamer":Player,
-    "Stuttering Judge":Player, 
-    "Rusted Sword Knight":Player, 
-    "Shaman":Player, 
-    "Puppeteer":Player, 
-    "Ankou":Player,
-    "Night Owl":Player
-    "Astronomer":Player
-    }
+    "Villager": Villager,
+    "Villager-Villager": Player,
+    "Fortune Teller": FortuneTeller,
+    "Cupid": Player,
+    "Witch": Player,
+    "Hunter": Player,
+    "Little Girl": Player,
+    "Guard": Player,
+    "Ancient": Player,
+    "Scapegoat": Player,
+    "Idiot": Player,
+    "Savior": Player,
+    "Two Sisters": Player,
+    "Three Brothers": Player,
+    "Devoted Maid": Player,
+    "Fox": Player,
+    "Bear Tamer": Player,
+    "Stuttering Judge": Player,
+    "Rusted Sword Knight": Player,
+    "Shaman": Player,
+    "Puppeteer": Player,
+    "Ankou": Player,
+    "Night Owl": Player,
+    "Astronomer": Player,
+}
 
 
 werewolfRoles = {
-    "Werewolf":Werewolf,
-    "Big Bad Wolf":Player,
-    "Vile Father of Wolves":Player
-    "Fortune Teller Wolf": Player
-    }
+    "Werewolf": Werewolf,
+    "Big Bad Wolf": Player,
+    "Vile Father of Wolves": Player,
+    "Fortune Teller Wolf": Player,
+}
 ambiguousRoles = {
-    "Thief":Player,
-    "Devoted Maid":Player,
-    "Actor":Player,
-    "Wild Child":Player,
-    "Wolf-Dog":Player,
-    "Abominable Sectarian":Player
-    }
+    "Thief": Player,
+    "Devoted Maid": Player,
+    "Actor": Player,
+    "Wild Child": Player,
+    "Wolf-Dog": Player,
+    "Abominable Sectarian": Player,
+}
 lonerRoles = {
-    "White Wolf":Player,
-    "Angel":Player,
-    "Pied Pipper":Player,
-    "White Rabbit":Player
-    }
-specialRoles = {
-    "Pyromaniac":Player,
-    "Raven":Player,
-    "Gypsy":Player}
+    "White Wolf": Player,
+    "Angel": Player,
+    "Pied Pipper": Player,
+    "White Rabbit": Player,
+}
+specialRoles = {"Pyromaniac": Player, "Raven": Player, "Gypsy": Player}
